2e3bdafe616fe7551eba612b4b2d3db9a5add6cb
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2023-03-15 16:09:51 +0100

    fix(gha): keep comments in plugins.txt

--- bin/update-plugins.sh
+++ bin/update-plugins.sh
@@ -23,6 +23,16 @@ java -jar "${TMP_DIR}/jenkins-plugin-manager.jar" -f plugins.txt --available-upd
 
 mv plugins2.txt plugins.txt
 
+# Generate a patch without comments removal(s)
+# the first sed removes the comments removal(s) from the patch
+# the second sed fixes the patch by keeping the same amount of context before and after, since plugins are only updated and not removed with this script
+git diff  -p --stat plugins.txt | sed -r 's/-#/ #/' | sed -r 's/@@ -([0-9]+),([0-9]+) \+([0-9]+),[0-9]+/@@ -\1,\2 +\3,\2/' > patch-plugin-only.txt
+# Restore plugins.txt and apply the modified patch
+git restore plugins.txt
+git apply patch-plugin-only.txt
+# Remove the patch
+rm patch-plugin-only.txt
+
 git diff plugins.txt
 
 echo "Updating plugins complete"
